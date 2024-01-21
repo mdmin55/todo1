f1b047731d77a9b91d0cac2ede151705b24ae210
mdmin55 <145227034+mdmin55@users.noreply.github.com>
2024-01-21 22:54:46 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		colors: {
+			main: '#d3dfd0',
+			hover: '#d8d0d2',
+			
+		},
+	},
+	plugins: [],
+}
